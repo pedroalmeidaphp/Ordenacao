d7193a65d06d378c81941dea183aacbcdd9df39f
Pedro Henrique <103169173+pedrophp24072021@users.noreply.github.com>
2022-12-12 21:29:46 -0300

    Update TR3_537674.c

--- TR3_537674.c
+++ TR3_537674.c
@@ -15,30 +15,30 @@ int main(){
    	int i;
    	
    	//#########################
-	for (i = 0; i < 200; i++){
+	for (i = 0; i < 200; i++){     //preenche todos os espaços do vetor com 0
 		x[i] = 0;
 	}
 	
-   	while (1)
+   	while (1)//while true 
    	{
    		printf("Digite um numero de 0 a 5000.OBS: para exibi-lo na tela digite -1 ");
 		scanf("%d", &w);
 
-		if (w == -1)break;
+		if (w == -1)break; //faz com que o programa pare quando digitado -1 pelo usuario
 
 		if (w >= 0 && w<=5000)
 		{
 			j = w % 32;
-			k = w / 32;
+			k = w / 32; // pega a divisão e o resto do numero digitado por 32(numero de bits)
 
-			x[k] = (1 << j) | x[k]; 
+			x[k] = (1 << j) | x[k]; // coloca na posição k do vetor a comparação bit a bit entre 1 com e jota bits deslocados a esqueda e essa mesma posição( no caso um ou bit a bit) 
 		}
 		
 		else printf("Valor invalido \n");
 		}
 	printf("Os numeros sao: ");
 	for (i = 0; i < 200; i++){
-		for (z = 0; z < 32; z++){
+		for (z = 0; z < 32; z++){ //printa os numeros do vetor de forma ordenada, usando comparação de bits
         	if ((x[i] >> z) & 1) printf("%d ", z + i * 32);
     }
 	}
